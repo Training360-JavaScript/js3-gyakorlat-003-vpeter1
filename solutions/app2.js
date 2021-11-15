d9229ca1774b727a16d5fee3a6a479d9f97682ea
vargapeter66 <varga.peter.mobil@gmail.com>
2021-11-15 17:42:33 +0100

    v2?

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,3 +1,9 @@
+const setCookie = (string) => {
+  const now = new Date();
+  now.setTime(now.getTime() + (15 * 60 * 1000));
+  const expires = now.toUTCString();
+  document.cookie = `token=${string};expires=${expires};path=/;`;
+}
 
 
 
